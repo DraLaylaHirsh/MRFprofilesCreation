60a1f8202bfd0f27b26ed3dd221bce14aca920ed
DraLaylaHirsh <111517699+DraLaylaHirsh@users.noreply.github.com>
2022-11-24 18:05:55 +0100

    Update and rename runLinear.sh to runLinearProfile.sh

--- runLinearProfile.sh
+++ runLinearProfile.sh
@@ -17,7 +17,7 @@ echo $NAME
 
 pfw N=2000 $NAME".msf"  > "pfw"$NAME.msf
 pfmake -1 "pfw"$NAME".msf" "blosum45.cmp" > $NAME".prf"
-pfsearch -f $NAME".prf" $ruta"pdb_seqres.txt" > "output_ali_"$NAME
+pfsearch -f $NAME".prf" $ruta"pdb_seqres.txt" > "searchCircularResults"$NAME
 /home/lhirsh/Documents/scaling/autoscaling.pl $NAME".prf" > "new"$NAME".prf"
-pfsearch -f "new"$NAME".prf" $path"pdb_seqres.txt" > "outputnew_ali_"$NAME
-python3 Verifyresults.py "output_ali_"$NAME "outputnew_ali_"$NAME $OUTPUTPATH $NAME 
+pfsearch -f "new"$NAME".prf" $path"pdb_seqres.txt" > "autoscalingsearchCircularResults"$NAME
+python3 Verifyresults.py "searchCircularResults"$NAME "autoscalingsearchCircularResults_"$NAME $OUTPUTPATH $NAME 
