f7c5b0d313281aaa732c81055f32b02304231552
DraLaylaHirsh <111517699+DraLaylaHirsh@users.noreply.github.com>
2022-11-28 10:46:27 +0100

    Update runCircularProfile.sh

--- runCircularProfile.sh
+++ runCircularProfile.sh
@@ -3,15 +3,15 @@
 # and then create a new profile and run the verification against the results
 
 #path is where you locate the database for the pfsearch
-path="/home/lhirsh/Downloads/UpdateRepeatsDB/"
+path="/home/mypc/Downloads/"
 #NAME is the name of your msffile
 NAME=$1 #alignIV7
 #OUTPUTPATH is the path were the results will be saved and all the input data will be, this includes the blosum45
-OUTPUTPATH="/home/lhirsh/Documents/otras/"
+OUTPUTPATH="/home/mypc/Documents/otras/"
 
 pfw N=2000 $NAME".msf"  > "pfw"$NAME.msf
 pfmake -c "pfw"$NAME".msf" "blosum45.cmp" > $NAME".prf"
-pfsearch -f $NAME".prf" $ruta"pdb_seqres.txt" > "searchCircularResults_"$NAME
+pfsearch -f $NAME".prf" $path"pdb_seqres.txt" > "searchCircularResults_"$NAME
 /home/lhirsh/Documents/scaling/autoscaling.pl $NAME".prf" > "new"$NAME".prf"
-pfsearch -f "new"$NAME".prf" $path"pdb_seqres.txt" > "autoscalingsearchResults_"$NAME
+pfsearch -f "new"$NAME".prf" $path"pdb_seqres.txt" > "autoscalingsearchCircularResults_"$NAME
 python3 Verifyresults.py "searchResults_"$NAME "autoscalingsearchCircularResults_"$NAME $OUTPUTPATH $NAME 
