23857ad9d346a294e36bd48b90a1dd7f94868835
DraLaylaHirsh <111517699+DraLaylaHirsh@users.noreply.github.com>
2022-12-08 16:01:25 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@
 project = u'Final Report'
 copyright = u'2022, Layla Hirsh'
 author = u'Layla Hirsh'
-author = u['Layla Hirsh','P.I. Andrey Kajava']
+author = u'Dr. Layla Hirsh (Secondee), Dr. Andrey Kajava (Principal Investigator)'
 
 
 
